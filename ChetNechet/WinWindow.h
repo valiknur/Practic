532fd5dfc79e1685002e43af4e0ee39576e54186
valentine <101232960+valiknur@users.noreply.github.com>
2022-03-09 19:19:19 +0300

    Add files via upload

---
+++ ChetNechet/WinWindow.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "UnityWindow.h"
+class WinWindow :
+    public UnityWindow
+{
+public:
+    HWND staticTextForWinner;
+    HWND staticTextForNeChet;
+    HWND staticTextForChet;
+    HWND playButton;
+    HWND exitButton;
+    WinWindow(GameData* data);
+    PCWSTR  ClassName() const override { return L"Main"; }
+    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) override;
+    void ShowContext() override;
+};
+
